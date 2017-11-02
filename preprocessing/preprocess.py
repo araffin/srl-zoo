79cc949d34101cb66c754a30dbb634ba403b7044
Antonin RAFFIN <antonin.raffin@ensta-paristech.fr>
2017-11-02 16:03:39 +0100

    Add image preprocessing

--- preprocessing/preprocess.py
+++ preprocessing/preprocess.py
@@ -1,19 +1,21 @@
 """
 Preprocessing script to extract actions, rewards, ground truth from text files
 
+TODO: improve image preprocessing speed, reduce memory usage
 """
 from __future__ import print_function, division, absolute_import
 
+import argparse
 import os
-from collections import OrderedDict
 
+from tqdm import tqdm
+import cv2
 import pandas as pd
 import numpy as np
 
-from .utils import detectBasePath
+from .utils import detectBasePath, getActions, findClosestAction, getDataFrame, preprocessInput
 
 base_path = detectBasePath(__file__)
-experiment = "static_arm_movingButton"
 text_files = {
     'is_pressed': 'recorded_button1_is_pressed.txt',
     'button_position': 'recorded_button1_position.txt',
@@ -27,6 +29,10 @@ N_ACTIONS = 26
 # Bound for negative rewards
 BOUND_INF = [0.42, -0.1, -0.11]
 BOUND_SUP = [0.75, 0.60, 0.35]
+# Resized image shape
+IMAGE_WIDTH = 16 # in px
+IMAGE_HEIGHT = 16 # in px
+N_CHANNELS = 3
 
 def isInBound(coordinate):
     """
@@ -39,109 +45,113 @@ def isInBound(coordinate):
     return True
 
 
-def getActions(delta_pos, n_actions):
-    """
-    :param delta_pos: (float)
-    :param n_actions: (int)
-    :return: (dict)
-    """
-    possible_deltas = [i * delta_pos for i in range(-1, 2)]
-    actions = []
-    for dx in possible_deltas:
-        for dy in possible_deltas:
-            for dz in possible_deltas:
-                if dx == 0 and dy == 0 and dz == 0:
-                    continue
-                actions.append((dx, dy, dz))
-
-    assert len(actions) == n_actions, "Wrong number of actions: {}".format(len(actions))
-
-    action_to_idx = {action: idx for idx, action in enumerate(actions)}
-    # Sort the dictionnary to have a consistent order
-    action_to_idx = OrderedDict(sorted(action_to_idx.items(), key=lambda item: item[1]))
-
-    return action_to_idx
-
-
-def findClosestAction(action, action_to_idx, show_warning=True):
-    """
-    :param action: ([float])
-    :param action_to_idx: (dict)
-    :param show_warning: (bool)
-    :return: (int)
-    """
-    action_idx = action_to_idx.get(tuple(action))
-    if action_idx is None:
-        if show_warning:
-            print("[WARNING] {} not found in action dict".format(action))
-        distances = [np.linalg.norm(np.array(a) - action) for a in action_to_idx.keys()]
-        action_idx = np.argmin(distances)
-    return action_idx
-
-
-def getDataFrame(text_file):
-    """
-    :param text_file: (str) path to a text file extracted from a ROS bag
-    :return: (pandas dataFrame)
-    """
-    with open(text_file) as f:
-        # Read first line and retrieve the column names
-        headers = f.readline().strip("#").strip("\n")[1:].split(' ')
-    return pd.read_csv(text_file, sep=" ", skiprows=1, names=headers)
-
-
 if __name__ == '__main__':
-    data_folder = "{}/data/{}/".format(base_path, experiment)
-    record_folders = os.listdir(data_folder)
+    parser = argparse.ArgumentParser(description='Preprocess extracted ros bags')
+    parser.add_argument('--experiment', type=str, default="", help='Experiment name')
+    parser.add_argument('--mode', type=str, default="tf", help='Preprocessing mode: One of "caffe", "tf".')
+    parser.add_argument('--no-warnings', action='store_true', default=False,
+                        help='disables warnings')
+    args = parser.parse_args()
+
+    assert args.experiment != "", "You must specify an experiment"
+    assert args.mode in ['tf', 'caffe'], "Unknown mode"
+    
+    print("Experiment: {}".format(args.experiment))
+    print("Mode: {}".format(args.mode))
+    print("Resized shape: ({}, {})".format(IMAGE_WIDTH, IMAGE_HEIGHT))
+
+    experiment_name = args.experiment
+    data_folder = "{}/data/{}/".format(base_path, experiment_name)
+    record_folders = [item for item in os.listdir(data_folder) if os.path.isdir('{}/{}'.format(data_folder, item))]
     # Sort folders
     record_folders.sort(key=lambda item: int(item.split('_')[1]))
-    print("Found {} folder(s)".format(len(record_folders)))
-    # Iterate through record folders
-    record_folder = '{}/{}'.format(data_folder, record_folders[0])
-    image_folders = [item for item in os.listdir(record_folder) if os.path.isdir('{}/{}'.format(record_folder, item))]
 
-    # Retrieve frame indices where the button was pressed
-    df = getDataFrame('{}/{}'.format(record_folder, text_files['is_pressed']))
-    rewards = df['value'].values
-
-    # Retrieve button position
-    with open('{}/{}'.format(record_folder, text_files['button_position'])) as f:
-        button_position = map(float, f.readlines()[1].split(' '))
-
+    all_actions, all_rewards, episodes_starts = None, None, None
+    button_positions, all_arm_states, all_observations = [], None, None
     action_to_idx = getActions(DELTA_POS, N_ACTIONS)
 
-    # Retrieve arm actions
-    df = getDataFrame('{}/{}'.format(record_folder, text_files['arm_action']))
-    actions = []
-    n_frames = len(df)
-    for i in range(n_frames):
-        delta_action = map(float, (df.dx[i], df.dy[i], df.dz[i]))
-        actions.append(findClosestAction(tuple(delta_action), action_to_idx))
-    actions = np.array(actions)
+    print("Found {} folder(s)".format(len(record_folders)))
+    # Iterate through record folders
+    pbar = tqdm(total=len(record_folders))
+    for record_folder_name in record_folders:
+        record_folder = '{}/{}'.format(data_folder, record_folder_name)
+        image_folders = [item for item in os.listdir(record_folder) if os.path.isdir('{}/{}'.format(record_folder, item))]
 
-    # Retrieve ground truth states:
-    df = getDataFrame('{}/{}'.format(record_folder, text_files['arm_state']))
-    arm_states = []
-    for i in range(n_frames):
-        arm_states.append(map(float, (df.x[i], df.y[i], df.z[i])))
-    arm_states = np.array(arm_states)
+        assert len(image_folders) == 1, "Multiple image folders are not supported yet"
+        images = [item for item in os.listdir('{}/{}/'.format(record_folder, image_folders[0])) if item.endswith(".jpg")]
+        observations = np.zeros((len(images), IMAGE_WIDTH, IMAGE_HEIGHT, N_CHANNELS))
+        for idx, image in enumerate(images):
+            im = cv2.imread('{}/{}/{}'.format(record_folder, image_folders[0], image))
+            im = cv2.resize(im, (IMAGE_WIDTH, IMAGE_HEIGHT), interpolation=cv2.INTER_AREA)
+            obs = preprocessInput(im.astype(np.float32), mode=args.mode)
+            obs = np.expand_dims(obs, axis=0)
+            observations[idx] = obs
 
-    # Add negative rewards
-    for i in range(n_frames):
-        if rewards[i] > 0:
-            continue
-        if not isInBound(arm_states[i]):
-            rewards[i] = -1
+        # Retrieve frame indices where the button was pressed
+        df = getDataFrame('{}/{}'.format(record_folder, text_files['is_pressed']))
+        rewards = df['value'].values
 
-    print('{} positive rewards, {} negative rewards'.format(sum(rewards > 0), sum(rewards < 0)))
+        # Retrieve button position
+        with open('{}/{}'.format(record_folder, text_files['button_position'])) as f:
+            button_position = map(float, f.readlines()[1].split(' '))
 
+        # Retrieve arm actions
+        df = getDataFrame('{}/{}'.format(record_folder, text_files['arm_action']))
+        actions = []
+        n_frames = len(df)
+        for i in range(n_frames):
+            delta_action = map(float, (df.dx[i], df.dy[i], df.dz[i]))
+            actions.append(findClosestAction(tuple(delta_action), action_to_idx, not args.no_warnings))
+        actions = np.array(actions)
+
+        # Retrieve ground truth states:
+        df = getDataFrame('{}/{}'.format(record_folder, text_files['arm_state']))
+        arm_states = []
+        for i in range(n_frames):
+            arm_states.append(map(float, (df.x[i], df.y[i], df.z[i])))
+        arm_states = np.array(arm_states)
+
+        # Add negative rewards
+        for i in range(n_frames):
+            if rewards[i] > 0:
+                continue
+            if not isInBound(arm_states[i]):
+                rewards[i] = -1
+
+        # print('{} positive rewards, {} negative rewards'.format(sum(rewards > 0), sum(rewards < 0)))
+        episode_start = np.zeros(len(rewards))
+        episode_start[0] = 1
+        button_positions.append(button_position)
+
+        if all_actions is None:
+            all_actions = actions
+            all_rewards = rewards
+            episode_starts = episode_start[:]
+            all_arm_states = arm_states
+            all_observations = [observations]
+        else:
+            all_actions = np.concatenate((all_actions, actions), axis=0)
+            all_rewards = np.concatenate((all_rewards, rewards), axis=0)
+            episode_starts = np.concatenate((episode_starts, episode_start), axis=0)
+            all_arm_states = np.concatenate((all_arm_states, arm_states), axis=0)
+            all_observations.append(observations)
+        # Update progressbar
+        pbar.update(1)
+
+    all_observations = np.concatenate(all_observations)
+    pbar.close()
     # Save Everything
     data = {
-        'rewards': rewards,
-        'button_position': button_position,
-        'actions': actions,
+        'observations': all_observations,
+        'rewards': all_rewards,
+        'actions': all_actions,
         'actions_deltas': action_to_idx.keys(),
-        'arm_states': arm_states
     }
     print("Saving preprocessed data...")
-    np.savez('{}/data.npz'.format(record_folder), **data)
+    np.savez('{}/preprocessed_data.npz'.format(data_folder), **data)
+
+    ground_truth = {
+        'button_positions': button_positions,
+        'arm_states': all_arm_states
+    }
+    np.savez('{}/ground_truth.npz'.format(data_folder), **ground_truth)
--- preprocessing/utils.py
+++ preprocessing/utils.py
@@ -2,7 +2,11 @@ from __future__ import print_function, division
 
 import os
 import re
+from collections import OrderedDict
 
+import cv2
+import numpy as np
+import pandas as pd
 
 def detectBasePath(filename, folder_name="srl-robotic-priors-pytorch", default_path=""):
     """
@@ -21,3 +25,83 @@ def detectBasePath(filename, folder_name="srl-robotic-priors-pytorch", default_p
     else:
         print("[ERROR] Base path not found, fallback to default_path: {}".format(default_path))
     return base_path
+
+
+def getActions(delta_pos, n_actions):
+    """
+    :param delta_pos: (float)
+    :param n_actions: (int)
+    :return: (dict)
+    """
+    possible_deltas = [i * delta_pos for i in range(-1, 2)]
+    actions = []
+    for dx in possible_deltas:
+        for dy in possible_deltas:
+            for dz in possible_deltas:
+                if dx == 0 and dy == 0 and dz == 0:
+                    continue
+                actions.append((dx, dy, dz))
+
+    assert len(actions) == n_actions, "Wrong number of actions: {}".format(len(actions))
+
+    action_to_idx = {action: idx for idx, action in enumerate(actions)}
+    # Sort the dictionnary to have a consistent order
+    action_to_idx = OrderedDict(sorted(action_to_idx.items(), key=lambda item: item[1]))
+
+    return action_to_idx
+
+
+def findClosestAction(action, action_to_idx, show_warning=True):
+    """
+    :param action: ([float])
+    :param action_to_idx: (dict)
+    :param show_warning: (bool)
+    :return: (int)
+    """
+    action_idx = action_to_idx.get(tuple(action))
+    if action_idx is None:
+        if show_warning:
+            print("[WARNING] {} not found in action dict".format(action))
+        distances = [np.linalg.norm(np.array(a) - action) for a in action_to_idx.keys()]
+        action_idx = np.argmin(distances)
+    return action_idx
+
+
+def getDataFrame(text_file):
+    """
+    :param text_file: (str) path to a text file extracted from a ROS bag
+    :return: (pandas dataFrame)
+    """
+    with open(text_file) as f:
+        # Read first line and retrieve the column names
+        headers = f.readline().strip("#").strip("\n")[1:].split(' ')
+    return pd.read_csv(text_file, sep=" ", skiprows=1, names=headers)
+
+
+def preprocessInput(x, mode="caffe"):
+    """
+    :param x: (numpy tensor)
+    :param mode: (str) One of "caffe", "tf".
+        - caffe: will convert the images from RGB to BGR,
+            then will zero-center each color channel with
+            respect to the ImageNet dataset,
+            without scaling.
+        - tf: will scale pixels between -1 and 1,
+            sample-wise.
+    :return: (numpy tensor)
+    """
+    if mode == "tf":
+        x /= 255.
+        x -= 0.5
+        x *= 2.
+    elif mode == "caffe":
+        assert x.shape[-1] == 3, "Channel are not the last dimension"
+        # 'RGB'->'BGR'
+        x = x[..., ::-1]
+        # Zero-center by mean pixel
+        x[..., 0] -= 103.939
+        x[..., 1] -= 116.779
+        x[..., 2] -= 123.68
+    else:
+        raise ValueError("Unknown mode for preprocessing")
+    return x
