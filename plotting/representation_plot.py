429e535e986a249d234132c5407a91dab5c7378f
Antonin RAFFIN <antonin.raffin@ensta-paristech.fr>
2017-11-09 16:32:01 +0100

    Add ground truth plot

--- plotting/representation_plot.py
+++ plotting/representation_plot.py
@@ -1,10 +1,18 @@
 from __future__ import print_function, division
 
+import argparse
+
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import seaborn as sns
 import numpy as np
 from sklearn.decomposition import PCA
+
+# Python 2/3 compatibility
+try:
+    input = raw_input
+except NameError:
+    pass
 
 # Init seaborn
 sns.set()
@@ -75,3 +83,29 @@ def plot_observations(observations, name='Observation Samples'):
         plt.xticks([])
         plt.yticks([])
     plt.pause(0.0001)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Plotting script for representation')
+    parser.add_argument('-i', '--input_file', type=str, default="", help='Path to a npz file containing states and rewards')
+    parser.add_argument('--data_folder', type=str, default="", help='Path to a dataset folder, it will plot ground truth states')
+    args = parser.parse_args()
+
+    if args.input_file != "":
+        print("Loading {}...".format(args.input_file))
+        states_rewards = np.load(args.input_file)
+        plot_representation(states_rewards['states'], states_rewards['rewards'])
+        input('\nPress any key to exit.')
+
+    elif args.data_folder != "":
+        # Remove `data/` from the path if needed
+        if "data/" in args.data_folder:
+            args.data_folder = args.data_folder.split('data/')[1].strip("/")
+        print("Plotting ground truth...")
+        states = np.load('data/{}/ground_truth.npz'.format(args.data_folder))['arm_states']
+        rewards = np.load('data/{}/preprocessed_data.npz'.format(args.data_folder))['rewards']
+        name = "Ground Truth States - {}".format(args.data_folder)
+        plot_representation(states, rewards, name)
+        input('\nPress any key to exit.')
+
+    else:
+        print("You must specify one of --input_file or --data_folder")
